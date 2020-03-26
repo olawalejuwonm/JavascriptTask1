60165a9e88faf4a40e5461e0bdbf5c2eef330ceb
olawalejuwonm <olawalejuwon@gmail.com>
2020-03-26 16:58:21 +0100

    My Hng Javascript Task 1

---
+++ javascript1.js
@@ -0,0 +1,9 @@
+var my_name = "Micheal";
+var my_courses = ["html", "css", "javascript"];
+console.log(my_name, my_courses);
+var i;
+if (my_courses.length % 2 != 0) {
+	for (i = 1; i <= 200; i++) {
+		if (i % 2 != 0) {console.log(i)}
+}
+};
